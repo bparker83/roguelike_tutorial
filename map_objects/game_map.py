6b77dc2e42839a3de25fd753b1d3d29c770d8dc4
bparker83 <bparker83@hotmail.com>
2017-07-25 09:34:23 -0400

    Update game_map.py
    Updated Entity set to accept render_order param

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -6,6 +6,7 @@ from components.ai import BasicMonster
 from entity import Entity
 from map_objects.tile import Tile
 from map_objects.rectangle import Rect
+from render_functions import RenderOrder
 
 class GameMap:
 	def __init__(self, width, height):
@@ -99,12 +100,14 @@ class GameMap:
 					fighter_component = Fighter(hp=10, defense=0, power=3)
 					ai_component = BasicMonster()
 					
-					monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True, fighter=fighter_component, ai=ai_component)
+					monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True,
+									render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
 				else:
 					fighter_component = Fighter(hp=16, defense=1, power=4)
 					ai_component = BasicMonster()
 					
-					monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True, fighter=fighter_component, ai=ai_component)
+					monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True,
+									render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
 					
 				entities.append(monster)
 		
